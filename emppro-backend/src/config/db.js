cc548bfd6fe0398561853cf156ebd7295e722543
Sandhya Prajapati <125721669+sandhya7039@users.noreply.github.com>
2026-08-21 11:10:16 +0530

    Fix Aiven MySQL connection

--- emppro-backend/src/config/db.js
+++ emppro-backend/src/config/db.js
@@ -1,3 +1,20 @@
+// const mysql = require('mysql2/promise');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   timezone: '+05:30' // Indian timezone
+// });
+
+// module.exports = pool;
 const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 
@@ -5,13 +22,21 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: process.env.DB_SSL === 'true'
+    ? {
+        rejectUnauthorized: false
+      }
+    : undefined,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  timezone: '+05:30' // Indian timezone
+  timezone: '+05:30'
 });
 
 module.exports = pool;
--- emppro-frontend/README.md
+++ emppro-frontend/README.md
@@ -1,3 +1,4 @@
+
 # EmpproFrontend
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 22.0.8.
